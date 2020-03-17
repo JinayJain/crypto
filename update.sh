a039074535a581d6d8ddb013facddf858baf166f
Jinay Jain <jinaybjain@gmail.com>
2020-03-17 14:36:56 -0400

    Move scripts to respective folders and change update.sh accordingly

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 # Used to update the scripts
 
 echo "Moving Python scripts to ~/.local/bin/"
-for file in *.py; do
+for file in scripts/*.py; do
     base=$(basename -s .py $file)
     cp $file ~/.local/bin/$base
 done
